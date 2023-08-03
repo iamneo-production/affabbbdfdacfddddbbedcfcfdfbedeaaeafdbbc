cb96c1bafb58a1d8162882a68f88a7560030bc6a
Iamneo <techadmin@iamneo.ai>
2023-08-03 15:51:05 +0000

    Backup Commit

--- reactapp/src/components/UI/Button/Button.js
+++ reactapp/src/components/UI/Button/Button.js
@@ -1,28 +1,13 @@
-import React from 'react'
-import { useState } from 'react';
+import React from 'react';
 
-interface ButtonProps {
-    onClick: () => void;
-    text : String;
-}
+const Button = ({ onClick, text }) => {
+    const buttonText = text === "" ? "Button Works" : text;
 
-const ButtonFunc = (text) => {
-    if(text === ""){
-        return "Button Works";
-    } else {
-        return text;
-    }
-}
-
-// const Button: React.FC<ButtonProps> = ({onClick, text}) => {
-//     return <>
-//     <button onClick= {onClick}>{ButtonFunc(text)}</button>
-//     </>;
-// };
-
-
-const Button = (ButtonProps) => {
-    return (<button onClick = {ButtonProps.onClick}>{ButtonProps.children}</button>);
-}
+    return (
+        <button onClick={onClick}>
+            {buttonText}
+        </button>
+    );
+};
 
 export default Button;
